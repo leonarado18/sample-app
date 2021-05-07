7f20073825e55ac454af7ca29cfd4d43b42b6aa2
Domingo Pelago <domingo.pelago@coreproc.ph>
2021-05-08 03:12:24 +0800

    first commit

---
+++ src/sample/User.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class User {
+	
+	             
+	public static void main(String[] args) {
+		int[] users = { 1,2, 3};
+		
+		for(int user : users) {
+			System.out.println(user);
+		}
+	}
+	
+}
