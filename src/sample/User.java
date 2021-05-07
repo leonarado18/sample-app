94fba7df07391da7068d88deaa7be6eebcd69023
Domingo Pelago <domingo.pelago@coreproc.ph>
2021-05-08 03:29:12 +0800

    add 4 in users

--- src/sample/User.java
+++ src/sample/User.java
@@ -4,7 +4,7 @@ public class User {
 	
 	             
 	public static void main(String[] args) {
-		int[] users = { 1,2, 3};
+		int[] users = { 1,2, 3, 4};
 		
 		for(int user : users) {
 			System.out.println(user);
